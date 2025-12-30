a4988c58ec1a8580c5125c0c99a347bfae8378f0
Shibil2 <shammasshifu@gmail.com>
2025-12-30 14:21:47 +0530

    nw

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool islight = false;
   bool isliked = false;
+  bool isStared = false;
+  String countstar = "0";
+  String countheart = "0";
+  String countlight = "0";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,15 +34,25 @@ class _MyAppState extends State<MyApp> {
               onTap: () {
                 setState(() {
                   islight = !islight;
+                  countlight = islight ? "1" : "0";
                 });
               },
-              child: Icon(
-                islight
-                    ? Icons.lightbulb_rounded
-                    : Icons.lightbulb_outline_rounded,
+              child: Row(
+                children: [
+                  Icon(
+                    islight
+                        ? Icons.lightbulb_rounded
+                        : Icons.lightbulb_outline_rounded,
 
-                color: islight ? Colors.yellow : Colors.grey[700],
-                size: 100,
+                    color: islight ? Colors.yellow : Colors.grey[700],
+                    size: 100,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    countlight,
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 50),
@@ -45,12 +60,45 @@ class _MyAppState extends State<MyApp> {
               onTap: () {
                 setState(() {
                   isliked = !isliked;
+                  countheart = isliked ? "1" : "0";
                 });
               },
-              child: Icon(
-                isliked ? Icons.favorite : Icons.favorite_border,
-                color: isliked ? Colors.red : Colors.grey[700],
-                size: 100,
+              child: Row(
+                children: [
+                  Icon(
+                    isliked ? Icons.favorite : Icons.favorite_border,
+                    color: isliked ? Colors.red : Colors.grey[700],
+                    size: 100,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    countheart,
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isStared = !isStared;
+                  countstar = isStared ? "1" : "0";
+                });
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    isStared ? Icons.star : Icons.star_border,
+                    color: isStared ? Colors.blue : Colors.grey[700],
+                    size: 100,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    countstar,
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
           ],
